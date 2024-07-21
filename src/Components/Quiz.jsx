fe531272d3efe3d3ee2c63acbbf97219d7dfd01c
Hamza-ALyemeni <hamzaalchamak442979@gmail.com>
2024-07-21 17:59:06 +0300

    adding shuffling answers logic and summary div

--- src/Components/Quiz.jsx
+++ src/Components/Quiz.jsx
@@ -1,10 +1,13 @@
 import { useState } from "react"
 import QUESTIONS from "../questions.js"
+import trophyImage from "../assets/quiz-complete.png"
 
 export default function Quiz() {
     const [userAnswers , setUserAnswers] = useState([]);
 
     const actievQuistionIndex = userAnswers.length;
+   
+    const quizIsCompleted = actievQuistionIndex === QUESTIONS.length;
 
     function handleSelectAnswer(selectedAnswer) {
         setUserAnswers((prevSelectedAnswers) => {
@@ -12,12 +15,23 @@ export default function Quiz() {
         });
     }
 
+
+    if(quizIsCompleted){
+        return <div id="summary">
+            <img src={trophyImage} alt="Trophy Image" />
+            <h2>QUIZ COMPLETED</h2>
+        </div>
+    }
+    
+    const shuffledAnswers = [...QUESTIONS[actievQuistionIndex].answers];
+    shuffledAnswers.sort(() => Math.random - 0.5);
+
     return (
     <div id="quiz">
         <div id="quesiton">
             <h2>{QUESTIONS[actievQuistionIndex].text}</h2>
             <ul id="answers">
-                {QUESTIONS[actievQuistionIndex].answers.map(answer => (
+                {shuffledAnswers.map(answer => (
                 <li key={answer} className="answer">
                     <button onClick={() => handleSelectAnswer(answer)}>
                         {answer}
